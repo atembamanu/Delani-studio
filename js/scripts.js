e622b5c8e4ce77e53bf8d279d14cfdda1038cca0
atembamanu <atembaemm07@gmail.com>
2019-08-23 12:28:20 +0300

    create a business logic and handle clicks for the what we do sections

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+//user Interface logic
+
+//business logic
+
+$(function(){
+    $('#design').click(function(){
+        $('#design-content').show(function(){
+            $('#design').hide('fast');
+        });
+    });
+    $('#design-content').click(function(){
+        $('#design').show(function(){
+            $('#design-content').hide('fast');
+        });
+    });
+
+    $('#dev').click(function(){
+        $('#dev-content').show(function(){
+            $('#dev').hide('fast');
+        });
+    });
+    $('#dev-content').click(function(){
+        $('#dev').show(function(){
+            $('#dev-content').hide('fast');
+        });
+    });
+
+    $('#product').click(function(){
+        $('#product-content').show(function(){
+            $('#product').hide('fast');
+        });
+    });
+    $('#product-content').click(function(){
+        $('#product').show(function(){
+            $('#product-content').hide('fast');
+        });
+    });
+});
